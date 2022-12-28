96ea518ed1af03b3bc827f63f8f75a59d78c9a8e
Gokhan Serin <penmudamu@gmail.com>
2022-12-28 11:50:41 +0300

    day42 completed

--- src/day42_Exceptions/CustomExceptions.java
+++ src/day42_Exceptions/CustomExceptions.java
@@ -1,7 +1,17 @@
 package day42_Exceptions;
 
-class FadyExceptions extends RuntimeException{
+class FadyExceptions extends RuntimeException{ // custom unchecked exception
 
+    public FadyExceptions(){
+        super("Time for a short break");
+    }
+    public FadyExceptions(String message){
+        super(message);
+    }
+
+}
+
+class NoBreakException extends Exception{ // custom checked exception
 
 }
 
@@ -9,13 +19,15 @@ public class CustomExceptions {
 
     public static void main(String[] args) {
 
-        throw new FadyExceptions();
+        //throw new FadyExceptions("It's time for lunch break");
 
 
+        try {
+            throw new NoBreakException();
+        } catch (NoBreakException e) {
+            e.printStackTrace();
+        }
 
     }
-
-
-
 
 }
